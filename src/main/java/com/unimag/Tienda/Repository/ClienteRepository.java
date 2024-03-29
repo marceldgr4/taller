31faf004377491c5ab31b297b8d0c42bcb8be26b
marceldgr4 <marceldgr@gmail.com>
2024-03-29 02:19:39 -0500

    ClienteRepository PedidoRepository

--- src/main/java/com/unimag/Tienda/Repository/ClienteRepository.java
+++ src/main/java/com/unimag/Tienda/Repository/ClienteRepository.java
@@ -1,8 +1,16 @@
 package com.unimag.Tienda.Repository;
 import com.unimag.Tienda.Entidad.Cliente;
-import com.unimag.Tienda.Entidad.Producto;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+    List<Cliente> findByEmail(String email);
+    List<Cliente> findByDireccion(String Direccion);
+    List<Cliente> findByNombreStartingWithIgnoreCase(String Nombre);
+
+
 }
