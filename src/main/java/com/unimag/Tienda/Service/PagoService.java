28bb36f2cf149458fd48baac4326ab6edf5da201
hackintosh colombia <100010339+marceldgr@users.noreply.github.com>
2024-03-30 14:42:42 -0500

    PagoService correccion

--- src/main/java/com/unimag/Tienda/Service/PagoService.java
+++ src/main/java/com/unimag/Tienda/Service/PagoService.java
@@ -7,7 +7,10 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface PagoService {
-    static Pago crearPago(Pago pago);
+    static Pago crearPago(Pago pago) {
+        return null;
+    }
+
     List<Pago>obtenerTodoLosPagos();
     List<Pago>obtenerPagosPorRangoFecha(LocalDate startDate, LocalDate endDate);
     List<Pago>obtenerPagoPorOrdenYMetodoPago(Long idPedido, MetodoPago metodoPago);
