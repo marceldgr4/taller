c2fc431e079e0da2afdaad63c989b842253227f9
hackintosh colombia <100010339+marceldgr@users.noreply.github.com>
2024-03-30 22:59:06 -0500

    correction

--- src/main/java/com/unimag/Tienda/Entidad/Pedido.java
+++ src/main/java/com/unimag/Tienda/Entidad/Pedido.java
@@ -25,23 +25,15 @@ public class Pedido {
     @ManyToOne
     @JoinColumn(name = "idCliente")
     private Cliente cliente;
+
     private LocalDateTime fechaPedido;
     private Double total;
+
     @Enumerated(EnumType.STRING)
     private EstadoPedido status;
 
     @OneToMany(mappedBy = "pedido")
     private List<ItemPedido> itemPedidos;
-    @OneToOne(mappedBy = "pago")
-    private Pago pago;
+
+
 }
-/*
-@OneToMany(mappedBy = "pedido",cascade = CascadeType.ALL)
-    private List<ItemPedido> itemPedidos = new ArrayList<>();
-
-    @OneToOne(mappedBy = "pedido",cascade = CascadeType.ALL)
-    private Pago pago;
-
-    @OneToOne(mappedBy = "pedido",cascade = CascadeType.ALL)
-    private DetalleEnvio detalleEnvio;
-* */
